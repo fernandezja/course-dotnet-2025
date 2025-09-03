cfaf75ad4e237d2ba7a97b16ada9668aa57b492d
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-09-03 18:59:16 -0300

    [02-POO]

---
+++ 02-POO/UNNE.ConsoleAppDemoPOO/Materia.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNNE.ConsoleAppDemoPOO
+{
+    internal class Materia
+    {
+        //private string docente;
+
+        //public string GetDocente() { 
+        //    return docente;
+        //}
+
+        //public void SetDocente(string valor)
+        //{
+        //     docente = valor;
+        //}
+
+        public string Docente { get; set; }
+
+
+
+        private string _nombre;
+
+        public string Nombre
+        {
+            get { 
+                //Acciones
+                return _nombre;
+            }
+            set {
+                //Acciones
+                _nombre = value;
+                //Acciones
+            }
+        }
+
+
+        public string Formatear() {
+            return $"MATERIA: {Nombre}";
+        }
+    }
+}
---
+++ 02-POO/UNNE.ConsoleAppDemoPOO/Program.cs
@@ -0,0 +1,18 @@
+﻿using UNNE.ConsoleAppDemoPOO;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int anio = 2025;
+string demo = "2025";
+
+UNNE.ConsoleAppDemoPOO.Materia materia0;
+
+Materia materia;
+materia = new Materia();
+materia.Docente = "Profesor 1";
+materia.Nombre = "Programacion III";
+
+Console.WriteLine($"{materia.Formatear()}");
+
+Console.ReadKey();
