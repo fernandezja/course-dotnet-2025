41623161096d94550a2940bf890e9ac83439d002
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-10-01 18:39:40 -0300

    [07] Acceso a Datos con EF

---
+++ 07-AccesoADatosConEF/ConsoleApp7/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp6;
+
+Console.WriteLine("Acceso a Datos!");
+
+
+//1 - Conexion
+var connectionString = "Data Source=.;Initial Catalog=GestionUniversitaria;Integrated Security=true;TrustServerCertificate=True;";
+
+var conn = new Microsoft.Data.SqlClient.SqlConnection(connectionString);
+
+
+//2 - Comando
+var query = "SELECT MateriaId, Nombre FROM Materia WHERE Nombre LIKE '%status2%'";
+var command = new Microsoft.Data.SqlClient.SqlCommand();
+command.Connection = conn;
+command.CommandText = query;
+command.CommandType = System.Data.CommandType.Text;
+
+
+
+conn.Open();
+
+Console.WriteLine("Conexión abierta!");
+
+//3 - DataReader
+var reader = command.ExecuteReader();
+
+var materias = new List<Materia>();
+
+while (reader.Read())
+{
+    var materiaId = reader.GetInt32(0);
+    var nombre = reader.GetString(1);
+
+    materias.Add(new Materia { 
+                    MateriaId = materiaId, 
+                    Nombre = nombre 
+                });
+
+    //Console.WriteLine($"MateriaId: {materiaId}, Nombre: {nombre}");
+}
+
+
+
+conn.Close();
+Console.WriteLine("Conexión cerrada!");
+
+Console.WriteLine("Listado de materias: ", materias.Count());
+
+foreach (var materia in materias)
+{
+    Console.WriteLine($"MateriaId: {materia.MateriaId}, Nombre: {materia.Nombre}");
+}
