0ba5a704b2d91056fdfde4b332dbece3ba3a8ded
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-10-01 19:01:03 -0300

    [07] Refactorizamos el acceso a datos dentro de la libreria

--- 07-AccesoADatosConEF/ConsoleApp7/Program.cs
+++ 07-AccesoADatosConEF/ConsoleApp7/Program.cs
@@ -3,49 +3,9 @@ using GestionUniversitaria.Core.Datos.Entidades;
 
 Console.WriteLine("Acceso a Datos!");
 
+var repositorio = new GestionUniversitaria.Core.Datos.MateriaRepositorio();
 
-//1 - Conexion
-var connectionString = "Data Source=.;Initial Catalog=GestionUniversitaria;Integrated Security=true;TrustServerCertificate=True;";
-
-var conn = new Microsoft.Data.SqlClient.SqlConnection(connectionString);
-
-
-//2 - Comando
-var query = "SELECT MateriaId, Nombre FROM Materia WHERE Nombre LIKE '%status2%'";
-var command = new Microsoft.Data.SqlClient.SqlCommand();
-command.Connection = conn;
-command.CommandText = query;
-command.CommandType = System.Data.CommandType.Text;
-
-
-
-conn.Open();
-
-Console.WriteLine("Conexión abierta!");
-
-//3 - DataReader
-var reader = command.ExecuteReader();
-
-
-var materias = new List<Materia>();
-
-while (reader.Read())
-{
-    var materiaId = reader.GetInt32(0);
-    var nombre = reader.GetString(1);
-
-    materias.Add(new Materia { 
-                    MateriaId = materiaId, 
-                    Nombre = nombre 
-                });
-
-    //Console.WriteLine($"MateriaId: {materiaId}, Nombre: {nombre}");
-}
-
-
-
-conn.Close();
-Console.WriteLine("Conexión cerrada!");
+var materias = repositorio.ListadoFiltrado("status2");
 
 Console.WriteLine("Listado de materias: ", materias.Count());
 
---
+++ 07-AccesoADatosConEF/GestionUniversitaria.Core.Datos/MateriaRepositorio.cs
@@ -0,0 +1,59 @@
+﻿using GestionUniversitaria.Core.Datos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionUniversitaria.Core.Datos
+{
+    public class MateriaRepositorio
+    {
+
+        public List<Materia> ListadoFiltrado(string textoParaFiltrar) {
+
+            //1 - Conexion
+            var connectionString = "Data Source=.;Initial Catalog=GestionUniversitaria;Integrated Security=true;TrustServerCertificate=True;";
+
+            var conn = new Microsoft.Data.SqlClient.SqlConnection(connectionString);
+
+
+            //2 - Comando
+            var query = "SELECT MateriaId, Nombre FROM Materia WHERE Nombre LIKE '%status2%'";
+            var command = new Microsoft.Data.SqlClient.SqlCommand();
+            command.Connection = conn;
+            command.CommandText = query;
+            command.CommandType = System.Data.CommandType.Text;
+
+
+
+            conn.Open();
+
+            //3 - DataReader
+            var reader = command.ExecuteReader();
+
+
+            var materias = new List<Materia>();
+
+            while (reader.Read())
+            {
+                var materiaId = reader.GetInt32(0);
+                var nombre = reader.GetString(1);
+
+                materias.Add(new Materia
+                {
+                    MateriaId = materiaId,
+                    Nombre = nombre
+                });
+
+            }
+
+
+
+            conn.Close();
+
+
+            return materias;
+        }
+    }
+}
