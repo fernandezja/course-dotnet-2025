87f19485c06c88339b25a4b21a78ee52a660c810
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-10-01 18:53:38 -0300

    [07] Reorganizar el ns de Materias dentro de la carpeta Entidades(cambiar ns tambien)

--- 07-AccesoADatosConEF/ConsoleApp7/Program.cs
+++ 07-AccesoADatosConEF/ConsoleApp7/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using GestionUniversitaria.Core.Datos;
+using GestionUniversitaria.Core.Datos.Entidades;
 
 Console.WriteLine("Acceso a Datos!");
 
--- 07-AccesoADatosConEF/GestionUniversitaria.Core.Datos/Entidades/Materia.cs
+++ 07-AccesoADatosConEF/GestionUniversitaria.Core.Datos/Entidades/Materia.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GestionUniversitaria.Core.Datos
+namespace GestionUniversitaria.Core.Datos.Entidades
 {
     public class Materia
     {
