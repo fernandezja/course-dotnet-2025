29e1dd13ab86d35eac2c238a53e97ba59efd10a0
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-10-29 19:05:56 -0300

    [10-Blazor] Capa de Datos inicial para Guardar un alumno

---
+++ 10-Blazor/GestionUniversitaria.Core.Datos/AlumnoRepository.cs
@@ -0,0 +1,13 @@
+﻿using GestionUniversitaria.Core.Entidades;
+
+namespace GestionUniversitaria.Core.Datos
+{
+    public class AlumnoRepository
+    {
+        public bool Guardar(Alumno alumno)
+        {
+            // Lógica para guardar el alumno en la base de datos
+            return true; // Simulación de éxito
+        }
+    }
+}
