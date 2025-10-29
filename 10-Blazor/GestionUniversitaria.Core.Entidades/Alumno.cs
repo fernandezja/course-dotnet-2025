999752f977984528044ad0214897db677d30b1b0
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-10-29 18:34:06 -0300

    [10-Blazor] Init

---
+++ 10-Blazor/GestionUniversitaria.Core.Entidades/Alumno.cs
@@ -0,0 +1,12 @@
+﻿namespace GestionUniversitaria.Core.Entidades
+{
+    public class Alumno
+    {
+        public int AlumnoId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
